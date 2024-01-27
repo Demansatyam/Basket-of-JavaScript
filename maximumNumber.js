f4eae384deea94eda772ce0d33ed4ba7170b969b
Satyam Kumar <90399155+Demansatyam@users.noreply.github.com>
2024-01-27 20:51:46 +0530

    Add files via upload

---
+++ maximumNumber.js
@@ -0,0 +1,20 @@
+function findMaxNumber(arr) {
+    if (arr.length === 0) {
+      return undefined;
+    }
+  
+    let max = arr[0];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+  
+    return max;
+}
+
+const numbers = [5, 3, 9, 2, 7, 1];
+const result = findMaxNumber(numbers);
+console.log("The maximum number is:", result);
+  
